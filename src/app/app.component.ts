aa9f931a40eefd0a7292d3dc4899148513916350
Alejo Castañeda <fcastaneda@cidenet.com.co>
2024-03-09 22:22:21 -0500

    Simplified Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  template: `
+    <h1>Hola Mundo!</h1>
+  `,
 })
 export class AppComponent {
   title = 'Playground';
